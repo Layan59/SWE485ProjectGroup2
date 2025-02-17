e2d332bb584619df30b6d510b7d461993873e605
Layan <layanabualghaith@gmail.com>
2025-02-18 02:13:55 +0300

    modifing SVM (tuning)

--- Code/Supervised_SVM.py
+++ Code/Supervised_SVM.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Load the dataset
 X_train = pd.read_csv("X_train.csv")
@@ -14,8 +14,8 @@ y_test = pd.read_csv("y_test.csv")
 y_train = y_train.values.ravel()
 y_test = y_test.values.ravel()
 
-# Train the SVM model
-svm_model = SVC(kernel="linear", random_state=42)
+# Modify SVM hyperparameters to reduce overfitting
+svm_model = SVC(C=0.01, kernel="rbf", gamma=10, max_iter=500, random_state=42)
 svm_model.fit(X_train, y_train)
 
 # Make predictions
@@ -23,14 +23,16 @@ y_pred = svm_model.predict(X_test)
 
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
-print(f"\nSVM Model Accuracy: {accuracy:.4f}\n")
-print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print(f"\nModified SVM Accuracy: {accuracy:.4f}")
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
 
 # Confusion Matrix Visualization
 conf_matrix = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(12, 8))
-sns.heatmap(conf_matrix, annot=False, cmap="Blues", fmt="d")
+sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt="d")
 plt.xlabel("Predicted Label")
 plt.ylabel("Actual Label")
-plt.title("SVM Confusion Matrix")
+plt.title("Modified SVM Confusion Matrix")
 plt.show()
